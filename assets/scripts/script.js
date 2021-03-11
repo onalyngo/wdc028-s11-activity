381a0f038f8db479a4be7e4e2bbb61b7bee89eb3
Onalyn Go <onalyngo.dev@gmail.com>
2021-03-12 02:26:50 +0800

    initial commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+//ACTIVITY 10/16/2020 WDC028-S11-Activity
+
+function myCompleteAddress(addressSpecifics, barangay, city, province, country){
+	console.log(addressSpecifics, barangay, city, province, country);
+};
+
+myCompleteAddress('Unit J Teves Apartment,', 'Brgy. Putatan,', 'Muntinlupa city,', 'Metro Manila,', 'Philippines')
